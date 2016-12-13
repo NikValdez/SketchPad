f84a1ca88646af32a162850343456e8cefb5f6a3
NikValdez <nerdvestor@gmail.com>
2016-12-12 21:43:33 -0800

    Add files via upload
    First commits

---
+++ SketchPad/sketchPad.js
@@ -0,0 +1,51 @@
+var column = 16;
+var row = 16;
+
+
+$(document).ready(function() {
+$(function createGrid() {     
+    for (var i = 0; i < row; i++) {
+        for (var j = 0; j < column; j++) {
+        	$("<div>").addClass("squares").appendTo("#container"); 
+        }
+   	 
+    }
+});
+
+$(function draw() {	
+	$(".squares").hover(function(){
+	$(this).css("background-color", "blue");
+    });
+});
+
+$(".reset").click(function() {
+        $(".squares").css({"background-color" : "#FFF", "border-color" : "#FFF"});
+    });
+
+
+
+
+
+
+});
+
+
+ 
+
+
+
+
+
+// $(function clean() {
+// 	$(".squares").css;
+// });
+
+// $('.reset').click(function() {
+// 		clean();
+// 	});
+
+
+
+
+
+	
